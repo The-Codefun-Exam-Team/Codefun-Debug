f776d398b7911afa8a041f0055a214e9f7c7e50b
Tuấn Khanh Bùi <54497316+khuongdz@users.noreply.github.com>
2022-12-21 21:03:57 +0700

    change color

--- next-app/components/global.tsx
+++ next-app/components/global.tsx
@@ -9,15 +9,15 @@ function NavigationBar(): JSX.Element {
 	];
 
 	return (
-		<div className="fixed top-0 left-0 right-0 bg-blue-900 text-white pt-4 pb-3">
+		<div className="fixed top-0 left-0 right-0 pt-4 pb-3 bg-slate-200 text-gray-700 border-b-2 border-gray-400">
 			<div className="flex justify-between md:max-w-3xl max-w-4xl lg:max-w-5xl xl:max-w-6xl 2xl:max-w-7xl mx-auto px-2">
 				<Link href={"/"}>
-					<a className="text-3xl">Codefun Debug</a>
+					<a className="text-3xl font-semibold">Codefun Debug</a>
 				</Link>
-				<div className="hidden md:flex justify-around text-l divide-x-2 h-min my-auto">
+				<div className="hidden md:flex justify-around text-l divide-x-2 divide-gray-500 h-min my-auto">
 					{OptionList.map(([title, url]) => (
 						<Link href={url} key={title}>
-							<a className="px-4">{title}</a>
+							<a className="px-4 font-medium">{title}</a>
 						</Link>
 					))}
 				</div>
