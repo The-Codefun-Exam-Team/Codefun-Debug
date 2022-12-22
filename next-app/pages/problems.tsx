ad4989449fba6a77ef71041526c891298841dc20
Tuấn Khanh Bùi <54497316+khuongdz@users.noreply.github.com>
2022-12-22 10:06:24 +0700

    create placeholder

---
+++ next-app/pages/problems.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+
+/**
+ *
+ * @returns Problems page
+ * @url /problems
+ */
+const Problems: NextPage = () => {
+	return (
+		<>
+			<div>Problems page</div>
+		</>
+	);
+};
+
+export default Problems;
