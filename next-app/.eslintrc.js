169b35fb681f69198ee4ec514c0467ac071ad500
Tuấn Khanh Bùi <54497316+khuongdz@users.noreply.github.com>
2022-12-22 12:57:02 +0700

    resolve conflict

--- next-app/.eslintrc.js
+++ next-app/.eslintrc.js
@@ -6,7 +6,11 @@ module.exports = {
 	},
 	"extends": [
 		"eslint:recommended",
-		"plugin:@typescript-eslint/recommended"
+		"plugin:@typescript-eslint/recommended",
+		"next/core-web-vitals",
+		"next",
+		"prettier"
+
 	],
 	"overrides": [
 	],
