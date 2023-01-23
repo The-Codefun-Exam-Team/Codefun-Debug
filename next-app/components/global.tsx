58024bad90db68cb9258c24f5368c6e5dd13c13a
Tuấn Khanh Bùi <54497316+khuongdz@users.noreply.github.com>
2023-01-23 15:22:59 +0700

    fix responsive for navbar

--- next-app/components/global.tsx
+++ next-app/components/global.tsx
@@ -16,7 +16,7 @@ function NavigationBar(): ReactElement {
 
 	return (
 		<div className="sticky block top-0 left-0 right-0 pt-4 pb-3 bg-slate-200 text-gray-700 border-b-2 m-0 border-gray-400">
-			<div className="flex justify-between md:max-w-3xl max-w-4xl lg:max-w-5xl xl:max-w-6xl 2xl:max-w-7xl mx-auto px-2">
+			<div className="flex justify-between max-w-3xl md:max-w-4xl lg:max-w-5xl xl:max-w-6xl 2xl:max-w-7xl mx-auto px-2">
 				<Link href={"/"} passHref>
 					<a className="text-3xl font-semibold pt-1 mx-4">Codefun Debug</a>
 				</Link>
