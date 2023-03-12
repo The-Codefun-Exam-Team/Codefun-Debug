d29a70275fffdd965cc7322af1386422c98d3601
Tuấn Khanh Bùi <54497316+khuongdz@users.noreply.github.com>
2023-03-12 14:28:58 +0700

    hide password

--- next-app/pages/login.tsx
+++ next-app/pages/login.tsx
@@ -7,13 +7,16 @@ import type { NextPage } from "next";
  */
 const Login: NextPage = () => {
 	return (
-		<div className="w-full max-w-2xl mx-auto h-auto mt-[20vh] border-solid text-slate-800 border-slate-500 border-2 rounded-3xl">
-			<div className="text-2xl py-3 px-4 text-center  font-semibold">Please Login</div>
+		<div className="w-[90%] max-w-2xl mx-auto h-auto mt-[25vh] border-solid text-slate-700 border-slate-600 border-2 rounded-3xl">
+			<div className="text-2xl py-3 px-4 text-center font-semibold">Please login to continue</div>
 			<div className="flex justify-center">
-				<input type="text" className="my-1 w-[95%] px-2 py-1 text-lg rounded-md border-slate-500 border-2" placeholder="username" />
+				<input type="text" className="my-1 w-[95%] px-2 py-1 text-lg rounded-md border-slate-600 border-2" placeholder="username" />
 			</div>
 			<div className="flex justify-center">
-				<input type="text" className="my-2 w-[95%] px-2 py-1 text-lg rounded-md border-slate-500 border-2" placeholder="password" />
+				<input type="password" className="my-2 w-[95%] px-2 py-1 text-lg rounded-md border-slate-600 border-2" placeholder="password" />
+			</div>
+			<div className="flex justify-center">
+				<button type="submit" className="text-lg font-medium text-slate-700 border-slate-600 border-2 rounded-lg px-2 mb-2 mt-1 " >Login</button>
 			</div>
 		</div>
 	);
