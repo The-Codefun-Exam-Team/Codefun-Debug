2cfe33f1a896f945a230638acafc9a11ad77ce9e
Tuấn Khanh Bùi <54497316+khuongdz@users.noreply.github.com>
2023-05-06 18:54:07 +0700

    dropdown label using transition component

---
+++ src/components/NavigationBar/DropDown.tsx
@@ -0,0 +1,75 @@
+"use client"
+import { Transition } from "@headlessui/react";
+import { useState } from "react";
+
+export const DropDown = () => {
+	const [isShowing, setIsShowing] = useState(false);
+	return (
+		<>
+			<Transition as="span" unmount={true} show={!isShowing}>
+				<label
+				htmlFor="dropdown-check"
+				className="absolute float-right mr-4 inline-block h-9 w-10 cursor-pointer leading-[12px] md:hidden"
+				onClick={() => setIsShowing(!isShowing)}
+				>
+					<Transition.Child as ="span" className="inline-block h-[5px] w-full rounded-md bg-slate-800 align-middle"
+						enter="delay-300"
+						enterFrom="opacity-0"
+						enterTo="opacity-1"
+					>
+					</Transition.Child>
+					<Transition.Child as="span" className="inline-block h-[5px] w-full rounded-md bg-slate-800 align-middle"
+						enter="delay-300"
+						enterFrom="opacity-0"
+						enterTo="opacity-1"
+					>
+					</Transition.Child>
+					<Transition.Child as="span" className="inline-block h-[5px] w-full rounded-md bg-slate-800 align-middle"
+						enter="delay-300"
+						enterFrom="opacity-0"
+						enterTo="opacity-1"
+					>
+					</Transition.Child>
+				</label>
+			</Transition>
+
+			<Transition as="span" show={isShowing}
+			>
+				<label
+					htmlFor="dropdown-check"
+					className="absolute float-right mr-4 inline-block h-9 w-10 cursor-pointer leading-[12px] md:hidden"
+					onClick={() => setIsShowing(!isShowing)}
+					>
+					<Transition.Child as ="span" className="inline-block h-[5px] w-full rounded-md bg-slate-800 align-middle"
+						enter="transition-all ease-out-back duration-300"
+						enterFrom="translate-x-0 translate-y-0 rotate-0 origin-bottom-left"
+						enterTo="translate-x-[6.2px] translate-y-[-3px] rotate-45 origin-bottom-left"
+						leave="transition-all ease-out-back duration-300"
+						leaveFrom="translate-x-[6.2px] translate-y-[-3px] rotate-45 origin-bottom-left"
+						leaveTo="translate-x-0 translate-y-0 rotate-0 origin-bottom-left"
+						>
+						
+					</Transition.Child>
+					<Transition.Child as="span" className="inline-block h-[5px] w-full rounded-md bg-slate-800 align-middle"
+						enter="duration-200 transition-all ease-out"
+						enterFrom="opacity-1 w-full"
+						enterTo="opacity-0 w-[1px]"
+						leave="duration-200 transition-all ease-out"
+						leaveFrom="opacity-1 w-0"
+						leaveTo="opacity-1 w-full"
+						>
+					</Transition.Child>
+					<Transition.Child as="span" className="inline-block h-[5px] w-full rounded-md bg-slate-800 align-middle"
+						enter="transition-all ease-out-back duration-300"
+						enterFrom="translate-x-0 translate-y-0 rotate-0 origin-bottom-left"
+						enterTo="translate-x-[6px] translate-y-0 rotate-[-45deg] origin-bottom-left"
+						leave="transition-all ease-out-back duration-300"
+						leaveFrom="translate-x-[6px] translate-y-0 rotate-[-45deg] origin-bottom-left"
+						leaveTo="translate-x-0 translate-y-0 rotate-0 origin-bottom-left"
+						>
+					</Transition.Child>
+					</label>
+				</Transition>
+			</>
+			)
+};
--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 
+import { DropDown } from "./DropDown.js";
 import { Loginout } from "./Loginout.js";
 import { UserInfo } from "./UserInfo.js";
 
@@ -36,25 +37,7 @@ export const NavigationBar = () => (
         <Loginout />
       </div>
     </div>
-    <label
-      htmlFor="dropdown-check"
-      className="float-right mr-4 inline-block h-9 w-10 cursor-pointer leading-[12px] md:hidden
-				peer-checked:[&>:nth-child(1)]:translate-x-[6.2px]
-				peer-checked:[&>:nth-child(1)]:translate-y-[-3px]
-				peer-checked:[&>:nth-child(1)]:rotate-45
-
-				peer-checked:[&>:nth-child(2)]:w-1
-				peer-checked:[&>:nth-child(2)]:opacity-0
-				
-				peer-checked:[&>:nth-child(3)]:translate-x-[6px]
-				peer-checked:[&>:nth-child(3)]:rotate-[-45deg]
-				"
-    >
-      <span className="inline-block h-[5px] w-full origin-top-left rounded-md bg-slate-800 align-middle transition-all duration-300 ease-out-back" />
-      <span className="inline-block h-[5px] w-full rounded-md bg-slate-800 align-middle transition-all duration-300 ease-out-back" />
-      <span className="inline-block h-[5px] w-full origin-bottom-left rounded-md bg-slate-800 align-middle transition-all duration-300 ease-out-back" />
-    </label>
-
+    <DropDown />
     <div
       id="dropdown-content"
       className="flex h-0 w-full origin-top scale-y-0 flex-col opacity-0 transition-all duration-200 peer-checked:h-[136px] peer-checked:scale-y-100 peer-checked:opacity-100 md:hidden md:peer-checked:hidden"
