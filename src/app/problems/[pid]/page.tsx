9a6b81dcfbfbbae3c1595f71225374d1e0009075
Tuấn Khanh Bùi <54497316+khuongdz@users.noreply.github.com>
2023-05-11 22:58:53 +0700

    placeholder

--- src/app/problems/[pid]/page.tsx
+++ src/app/problems/[pid]/page.tsx
@@ -1,3 +1,16 @@
-const Page = () => <div>Problem Page</div>;
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Problem",
+};
+
+const Page = () => {
+	
+	return (
+		<div className="flex h-full w-full items-center self-start">
+			<span className=""></span>
+		</div>
+	)
+}
 
 export default Page;
