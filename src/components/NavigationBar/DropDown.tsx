e3ae57e17f183b22b36a6e5976fde993a42433b5
DuCanhGH <75556609+DuCanhGH@users.noreply.github.com>
2023-05-11 23:19:39 +0700

    chore: removed DropDown

--- src/components/NavigationBar/DropDown.tsx
+++ src/components/NavigationBar/DropDown.tsx
@@ -1,94 +1,14 @@
 "use client";
-import { Transition } from "@headlessui/react";
 import { useAppSelector } from "@redux/hooks";
 import { clsx } from "@utils/shared/index.js";
 import Link from "next/link";
-import { useState } from "react";
 
-import { LogInOut } from "./LogInOut.js";
 import { OPTION_LIST } from "./OPTION_LIST.js";
 import { UserInfo } from "./UserInfo.js";
 
-// Using transition from headlessui, but don't seem to fucking work
-export const DropDown = () => {
-  const [isShowing, setIsShowing] = useState(false);
-  return (
-    <>
-      <Transition as="span" unmount={true} show={!isShowing}>
-        <label
-          htmlFor="dropdown-check"
-          className="absolute float-right mr-4 inline-block h-9 w-10 cursor-pointer leading-[12px] md:hidden"
-          onClick={() => setIsShowing(!isShowing)}
-        >
-          <Transition.Child
-            as="span"
-            className="inline-block h-[5px] w-full rounded-md bg-slate-800 align-middle"
-            enter="delay-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-1"
-          />
-          <Transition.Child
-            as="span"
-            className="inline-block h-[5px] w-full rounded-md bg-slate-800 align-middle"
-            enter="delay-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-1"
-          />
-          <Transition.Child
-            as="span"
-            className="inline-block h-[5px] w-full rounded-md bg-slate-800 align-middle"
-            enter="delay-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-1"
-          />
-        </label>
-      </Transition>
-
-      <Transition as="span" show={isShowing}>
-        <label
-          htmlFor="dropdown-check"
-          className="absolute float-right mr-4 inline-block h-9 w-10 cursor-pointer leading-[12px] md:hidden"
-          onClick={() => setIsShowing(!isShowing)}
-        >
-          <Transition.Child
-            as="span"
-            className="inline-block h-[5px] w-full rounded-md bg-slate-800 align-middle"
-            enter="transition-all ease-out-back duration-300"
-            enterFrom="translate-x-0 translate-y-0 rotate-0 origin-bottom-left"
-            enterTo="translate-x-[6.2px] translate-y-[-3px] rotate-45 origin-bottom-left"
-            leave="transition-all ease-out-back duration-300"
-            leaveFrom="translate-x-[6.2px] translate-y-[-3px] rotate-45 origin-bottom-left"
-            leaveTo="translate-x-0 translate-y-0 rotate-0 origin-bottom-left"
-          />
-          <Transition.Child
-            as="span"
-            className="inline-block h-[5px] w-full rounded-md bg-slate-800 align-middle"
-            enter="duration-200 transition-all ease-out"
-            enterFrom="opacity-1 w-full"
-            enterTo="opacity-0 w-[1px]"
-            leave="duration-200 transition-all ease-out"
-            leaveFrom="opacity-1 w-0"
-            leaveTo="opacity-1 w-full"
-          />
-          <Transition.Child
-            as="span"
-            className="inline-block h-[5px] w-full rounded-md bg-slate-800 align-middle"
-            enter="transition-all ease-out-back duration-300"
-            enterFrom="translate-x-0 translate-y-0 rotate-0 origin-bottom-left"
-            enterTo="translate-x-[6px] translate-y-0 rotate-[-45deg] origin-bottom-left"
-            leave="transition-all ease-out-back duration-300"
-            leaveFrom="translate-x-[6px] translate-y-0 rotate-[-45deg] origin-bottom-left"
-            leaveTo="translate-x-0 translate-y-0 rotate-0 origin-bottom-left"
-          />
-        </label>
-      </Transition>
-    </>
-  );
-};
-
 // using css to animate the label
 // Well it do look suck but it fucking works
-export const DropDownCSS = () => {
+export const DropDown = () => {
   const { user, loading } = useAppSelector((state) => state.user);
   return (
     <>
@@ -124,7 +44,6 @@ export const DropDownCSS = () => {
           </Link>
         ))}
         <UserInfo />
-        <LogInOut />
       </div>
     </>
   );
--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -1,11 +1,9 @@
 import Link from "next/link";
 
-import { DropDownCSS } from "./DropDown.js";
-import { LogInOut } from "./LogInOut.js";
+import { DropDown } from "./DropDown.js";
 import { OPTION_LIST } from "./OPTION_LIST.js";
 import { UserInfo } from "./UserInfo.js";
 
-// TODO: use react dropdown instead of pure html css
 export const NavigationBar = () => (
   <nav className="sticky inset-x-0 top-0 m-0 block h-auto justify-between border-b-2 border-gray-400 bg-slate-200 py-3 text-slate-700">
     <div className="mx-auto inline-block max-w-3xl px-2 md:flex md:max-w-4xl md:justify-between lg:max-w-5xl xl:max-w-6xl 2xl:max-w-7xl">
@@ -19,7 +17,6 @@ export const NavigationBar = () => (
           </Link>
         ))}
         <UserInfo />
-        <LogInOut />
       </div>
       <div className="text-l my-auto hidden h-min justify-around divide-x-2 divide-gray-500 font-medium md:flex lg:hidden [&>*]:cursor-pointer [&>*]:px-4">
         {OPTION_LIST.map(([title, url, subtitle]) => (
@@ -28,9 +25,8 @@ export const NavigationBar = () => (
           </Link>
         ))}
         <UserInfo />
-        <LogInOut />
       </div>
     </div>
-    <DropDownCSS />
+    <DropDown />
   </nav>
 );
