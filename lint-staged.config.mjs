4f19c3ee6e32c9d146709e86b7e712620ad2fbc2
DuCanhGH <75556609+DuCanhGH@users.noreply.github.com>
2023-05-11 23:33:58 +0700

    chore: added lint-staged

---
+++ lint-staged.config.mjs
@@ -0,0 +1,46 @@
+/* eslint-disable tsdoc/syntax */
+// @ts-check
+import { ESLint } from "eslint";
+import { quote } from "shell-quote";
+
+const eslint = new ESLint();
+const isWin = process.platform === "win32";
+
+/**
+ * @type {Record<
+ *     string,
+ *     (filenames: string[]) => string | string[] | Promise<string | string[]>
+ * >}
+ */
+const config = {
+  "**/*.{js,jsx,cjs,mjs,ts,tsx}": (filenames) => {
+    const escapedFileNames = filenames
+      .map((filename) => (isWin ? filename : escapeStr([filename])))
+      .join(" ");
+    return [
+      `eslint --report-unused-disable-directives --fix ${filenames
+        .filter(async (file) => !(await eslint.isPathIgnored(file)))
+        .map((f) => `"${f}"`)
+        .join(" ")}`,
+      `prettier --write ${escapedFileNames}`,
+      `git add ${escapedFileNames}`,
+    ];
+  },
+  "**/*.{json,md,mdx,css,html,yml,yaml,scss}": (filenames) => {
+    const escapedFileNames = filenames
+      .map((filename) => (isWin ? filename : escapeStr([filename])))
+      .join(" ");
+    return [`prettier --write ${escapedFileNames}`, `git add ${escapedFileNames}`];
+  },
+};
+
+/**
+ * @param {string[]} str
+ * @returns
+ */
+function escapeStr(str) {
+  const escaped = quote(str);
+  return escaped.replace(/\\@/g, "@");
+}
+
+export default config;
