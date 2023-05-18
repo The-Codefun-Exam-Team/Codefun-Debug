1a4da5b32fa85d972cea4ac18a4c31b0deb53f61
Tuấn Khanh Bùi <54497316+khuongdz@users.noreply.github.com>
2023-05-18 09:05:09 +0700

    unused-vả

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,6 @@ import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
  
 export function middleware(request: NextRequest) {
-	const nextUrl = request.nextUrl;
 	if (!request.cookies.get('token')) {
 		return NextResponse.redirect(new URL('/beta/login', request.url));
 	}
