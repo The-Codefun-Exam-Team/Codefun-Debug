77fb0942adc5ef1a1564090a0c6b251854f6cdb1
Tuấn Khanh Bùi <54497316+khuongdz@users.noreply.github.com>
2023-05-20 21:35:16 +0700

    submitSchema

---
+++ src/shared/schemas/submitSchema.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+
+export const submitSchema = z.object({
+	problemID: z.string().startsWith("D"),
+	code: z.string(),
+})
+
+export type SubmitSchemaType = z.infer<typeof submitSchema>;
+
+export interface SubmitData {
+	submissionID: number;
+}
