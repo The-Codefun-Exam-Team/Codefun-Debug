8d4583b239df2704dfe1a1131951ba67b10d22f8
Tuấn Khanh Bùi <54497316+khuongdz@users.noreply.github.com>
2023-05-20 21:15:52 +0700

    TODO

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,7 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
+  // TODO: handle adding prev params to login redirect
   if (!request.cookies.get("token")) {
     return NextResponse.redirect(new URL("/beta/login", request.url),);
   }
