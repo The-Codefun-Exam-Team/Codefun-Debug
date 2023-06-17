a623260bbe803b50e5e1a7592f7b861ccf3f74cc
Tuấn Khanh Bùi <54497316+khuongdz@users.noreply.github.com>
2023-06-17 19:42:24 +0700

    parallel data fetching

--- src/app/rankings/[group]/[page]/page.tsx
+++ src/app/rankings/[group]/[page]/page.tsx
@@ -7,14 +7,9 @@ export const metadata: Metadata = {
   title: "Rankings",
 };
 
-const Page = async ({ params: { group, page } }: { params: { group: string; page: string } }) => {
-  // get token from cookie
-  const cookieStore = cookies();
-  const token = cookieStore.get("token");
-
-  // fetch data
-  const bodyData = { group: group, pageid: page, limit: "50" as string } as Record<string, string>;
-  const request = await fetch(
+async function getRankings(group: string, page: string, limit: string, token: string) {
+  const bodyData = { group: group, pageid: page, limit: limit } as Record<string, string>;
+  const requestRanking = await fetch(
     `https://debug.codefun.vn/api/rankings?${new URLSearchParams(bodyData).toString()}`,
     {
       method: "GET",
@@ -23,8 +18,32 @@ const Page = async ({ params: { group, page } }: { params: { group: string; page
       },
     },
   );
-  const data = (await request.json()) as Array<UserData>;
-  console.log(data);
+  const rankingData = (await requestRanking.json()) as Array<UserData>;
+  return rankingData;
+}
+
+async function getGroups(token: string) {
+  const requestGroups = await fetch("https://debug.codefun.vn/api/groups", {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const groupsData = (await requestGroups.json()) as Array<string>;
+  return groupsData;
+}
+
+const Page = async ({ params: { group, page } }: { params: { group: string; page: string } }) => {
+  // get token from cookie
+  const cookieStore = cookies();
+  const token = cookieStore.get("token");
+  if (token === undefined) {
+    return <div>Token not found</div>;
+  }
+
+  // fetch data
+  const rankingData = getRankings(group, page, "50", token.value);
+  const groupsData = getGroups(token.value);
 
   return <div>Rankings page</div>;
 };
