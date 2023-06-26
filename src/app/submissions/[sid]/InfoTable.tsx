f068bbcf1b6b41ee46fb0d67fa5c01436aa4ba5a
DuCanhGH <75556609+DuCanhGH@users.noreply.github.com>
2023-06-26 23:03:19 +0700

    polish

--- src/app/submissions/[sid]/InfoTable.tsx
+++ src/app/submissions/[sid]/InfoTable.tsx
@@ -39,7 +39,7 @@ export const InfoTable = ({
             className="divide-y-2 divide-slate-300 rounded-md border-[2px] border-slate-300 text-left text-lg font-semibold text-slate-700
             [&>div]:p-2 [&>div]:text-left"
           >
-            <div className="h-fit">
+            <div>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -47,6 +47,7 @@ export const InfoTable = ({
                 strokeWidth="1.5"
                 stroke="currentColor"
                 className="relative bottom-[3px] inline h-6 w-6"
+                aria-hidden
               >
                 <path
                   strokeLinecap="round"
@@ -59,7 +60,7 @@ export const InfoTable = ({
                 {runData.owner.name}
               </Link>
             </div>
-            <div className="h-fit">
+            <div>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -67,6 +68,7 @@ export const InfoTable = ({
                 strokeWidth="1.5"
                 stroke="currentColor"
                 className="relative bottom-[3px] inline h-6 w-6"
+                aria-hidden
               >
                 <path
                   strokeLinecap="round"
@@ -76,12 +78,13 @@ export const InfoTable = ({
               </svg>{" "}
               Submission ID: {sid}
             </div>
-            <div className="h-fit">
+            <div>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 20 20"
                 fill="currentColor"
                 className="relative bottom-[3px] inline h-6 w-6"
+                aria-hidden
               >
                 <path
                   fillRule="evenodd"
@@ -101,23 +104,24 @@ export const InfoTable = ({
                 </Link>
               }
             </div>
-            <div className="h-fit">
+            <div>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 20 20"
                 fill="currentColor"
                 className="relative bottom-[3px] inline h-6 w-6"
+                aria-hidden
               >
                 <path d="M7.75 2.75a.75.75 0 00-1.5 0v1.258a32.987 32.987 0 00-3.599.278.75.75 0 10.198 1.487A31.545 31.545 0 018.7 5.545 19.381 19.381 0 017 9.56a19.418 19.418 0 01-1.002-2.05.75.75 0 00-1.384.577 20.935 20.935 0 001.492 2.91 19.613 19.613 0 01-3.828 4.154.75.75 0 10.945 1.164A21.116 21.116 0 007 12.331c.095.132.192.262.29.391a.75.75 0 001.194-.91c-.204-.266-.4-.538-.59-.815a20.888 20.888 0 002.333-5.332c.31.031.618.068.924.108a.75.75 0 00.198-1.487 32.832 32.832 0 00-3.599-.278V2.75z" />
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M13 8a.75.75 0 01.671.415l4.25 8.5a.75.75 0 11-1.342.67L15.787 16h-5.573l-.793 1.585a.75.75 0 11-1.342-.67l4.25-8.5A.75.75 0 0113 8zm2.037 6.5L13 10.427 10.964 14.5h4.073z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 />
               </svg>{" "}
               Language: {runData.language}
             </div>
-            <div className="h-fit">
+            <div>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -125,6 +129,7 @@ export const InfoTable = ({
                 strokeWidth="1.5"
                 stroke="currentColor"
                 className="relative bottom-[2px] inline h-6 w-6"
+                aria-hidden
               >
                 <path
                   strokeLinecap="round"
