de91529e3721f16a49c9f3a2f099254832d2f83c
DuCanhGH <75556609+DuCanhGH@users.noreply.github.com>
2023-06-26 23:00:10 +0700

    polish

--- src/app/submissions/[sid]/InfoTable.tsx
+++ src/app/submissions/[sid]/InfoTable.tsx
@@ -16,7 +16,7 @@ export const InfoTable = ({
     <table className="w-full table-auto border-separate border-spacing-x-2 border-spacing-y-4 rounded-md border-2 border-slate-600">
       <thead>
         <tr>
-          {submissionData.edit_score !== (100 as number) ? (
+          {submissionData.edit_score < 100 ? (
             <th
               className="border-b-[1px] border-slate-300 pb-8 pt-6 text-2xl font-normal text-green-600"
               colSpan={2}
@@ -44,13 +44,13 @@ export const InfoTable = ({
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke-width="1.5"
+                strokeWidth="1.5"
                 stroke="currentColor"
                 className="relative bottom-[3px] inline h-6 w-6"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
                 />
               </svg>{" "}
@@ -64,13 +64,13 @@ export const InfoTable = ({
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke-width="1.5"
+                strokeWidth="1.5"
                 stroke="currentColor"
                 className="relative bottom-[3px] inline h-6 w-6"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
                 />
               </svg>{" "}
@@ -84,9 +84,9 @@ export const InfoTable = ({
                 className="relative bottom-[3px] inline h-6 w-6"
               >
                 <path
-                  fill-rule="evenodd"
+                  fillRule="evenodd"
                   d="M4.5 2A1.5 1.5 0 003 3.5v13A1.5 1.5 0 004.5 18h11a1.5 1.5 0 001.5-1.5V7.621a1.5 1.5 0 00-.44-1.06l-4.12-4.122A1.5 1.5 0 0011.378 2H4.5zm2.25 8.5a.75.75 0 000 1.5h6.5a.75.75 0 000-1.5h-6.5zm0 3a.75.75 0 000 1.5h6.5a.75.75 0 000-1.5h-6.5z"
-                  clip-rule="evenodd"
+                  clipRule="evenodd"
                 />
               </svg>{" "}
               Statement:{" "}
@@ -122,13 +122,13 @@ export const InfoTable = ({
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke-width="1.5"
+                strokeWidth="1.5"
                 stroke="currentColor"
                 className="relative bottom-[2px] inline h-6 w-6"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
                 />
               </svg>{" "}
