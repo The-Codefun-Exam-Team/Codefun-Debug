762b2f910b14c1fd8d599dcdcf81ae9cd80f59ae
Tuấn Khanh Bùi <54497316+khuongdz@users.noreply.github.com>
2023-07-01 19:24:31 +0700

    fix: border overflow

--- src/app/rankings/[group]/[page]/RankTable.tsx
+++ src/app/rankings/[group]/[page]/RankTable.tsx
@@ -23,58 +23,60 @@ export const RankTable = ({ rankingData, page }: { rankingData: RankingsData; pa
     );
   }
   return (
-    <table className="mt-8 w-full table-fixed border-separate border-spacing-0 rounded-lg border-2">
-      <thead>
-        <tr
-          className={clsx(
-            "h-fit bg-gray-300 text-sm font-semibold md:text-lg",
-            "[&>th>div]:overflow-hidden [&>th>div]:text-ellipsis [&>th>div]:break-words [&>th>div]:p-3",
-          )}
-        >
-          <th colSpan={RANK_TABLE_DIST[0]}>
-            <div>#</div>
-          </th>
-          <th colSpan={RANK_TABLE_DIST[1]}>
-            <div>Username</div>
-          </th>
-          <th colSpan={RANK_TABLE_DIST[2]}>
-            <div>Name</div>
-          </th>
-          <th colSpan={RANK_TABLE_DIST[3]}>
-            <div>Points</div>
-          </th>
-          <th colSpan={RANK_TABLE_DIST[4]}>
-            <div>Rank</div>
-          </th>
-        </tr>
-      </thead>
-      <tbody className="h-fit divide-y-2 divide-gray-200">
-        {rankingData.map((user, index) => (
+    <div className="mt-8 overflow-hidden rounded-md border-2 border-gray-300">
+      <table className="w-full table-fixed border-collapse border-spacing-0">
+        <thead>
           <tr
-            key={`ranking-page-${page}-user-${user.id}`}
             className={clsx(
-              "group h-10 text-center even:bg-gray-100",
-              "[&>td>div]:line-clamp-2 [&>td>div]:text-ellipsis [&>td>div]:break-words",
+              "h-fit bg-gray-300 text-sm font-semibold md:text-lg",
+              "[&>th>div]:overflow-hidden [&>th>div]:text-ellipsis [&>th>div]:break-words [&>th>div]:p-3",
             )}
           >
-            <td colSpan={RANK_TABLE_DIST[0]}>
-              <div>{(+page - 1) * 50 + index + 1}</div>
-            </td>
-            <td colSpan={RANK_TABLE_DIST[1]}>
-              <div>{user.username}</div>
-            </td>
-            <td colSpan={RANK_TABLE_DIST[2]}>
-              <div>{user.name}</div>
-            </td>
-            <td colSpan={RANK_TABLE_DIST[3]}>
-              <div>{Math.round(user.points * 100) / 100}</div>
-            </td>
-            <td colSpan={RANK_TABLE_DIST[4]}>
-              <div>{user.rank}</div>
-            </td>
+            <th colSpan={RANK_TABLE_DIST[0]}>
+              <div>#</div>
+            </th>
+            <th colSpan={RANK_TABLE_DIST[1]}>
+              <div>Username</div>
+            </th>
+            <th colSpan={RANK_TABLE_DIST[2]}>
+              <div>Name</div>
+            </th>
+            <th colSpan={RANK_TABLE_DIST[3]}>
+              <div>Points</div>
+            </th>
+            <th colSpan={RANK_TABLE_DIST[4]}>
+              <div>Rank</div>
+            </th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody className="h-fit divide-y-2 divide-gray-200">
+          {rankingData.map((user, index) => (
+            <tr
+              key={`ranking-page-${page}-user-${user.id}`}
+              className={clsx(
+                "group h-10 text-center even:bg-gray-100",
+                "[&>td>div]:line-clamp-2 [&>td>div]:text-ellipsis [&>td>div]:break-words",
+              )}
+            >
+              <td colSpan={RANK_TABLE_DIST[0]}>
+                <div>{(+page - 1) * 50 + index + 1}</div>
+              </td>
+              <td colSpan={RANK_TABLE_DIST[1]}>
+                <div>{user.username}</div>
+              </td>
+              <td colSpan={RANK_TABLE_DIST[2]}>
+                <div>{user.name}</div>
+              </td>
+              <td colSpan={RANK_TABLE_DIST[3]}>
+                <div>{Math.round(user.points * 100) / 100}</div>
+              </td>
+              <td colSpan={RANK_TABLE_DIST[4]}>
+                <div>{user.rank}</div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
