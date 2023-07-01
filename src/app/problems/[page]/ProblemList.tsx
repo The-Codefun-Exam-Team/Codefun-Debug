04ee6fe8afe4554b7add172a930f0e5cb7bcd9d2
Tuấn Khanh Bùi <54497316+khuongdz@users.noreply.github.com>
2023-07-01 19:39:33 +0700

    text-align

--- src/app/problems/[page]/ProblemList.tsx
+++ src/app/problems/[page]/ProblemList.tsx
@@ -5,11 +5,11 @@ import type { DebugProblemBrief } from "./types";
 export const ProblemsList = ({ data, page }: { data: DebugProblemBrief[]; page: string }) => {
   return (
     <div className="w-full overflow-hidden rounded-md border-2 border-gray-300">
-      <table className="w-full overflow-hidden">
+      <table className="w-full table-auto overflow-hidden">
         <thead>
-          <tr className="bg-gray-300 text-lg font-semibold [&>th]:py-3">
-            <th> Problem </th>
-            <th> Language </th>
+          <tr className="bg-gray-300 text-lg font-semibold [&>th]:text-ellipsis [&>th]:break-words [&>th]:py-3">
+            <th className="text-left"> Problem </th>
+            <th className="text-left"> Language </th>
             <th> Score </th>
           </tr>
         </thead>
@@ -19,14 +19,14 @@ export const ProblemsList = ({ data, page }: { data: DebugProblemBrief[]; page: 
               key={`problem-page-${page}-code-${problem.code}`}
               className={clsx(
                 "h-10 text-center even:bg-gray-100",
-                "[&>td>div]:line-clamp-2 [&>td>div]:text-ellipsis [&>td>div]:break-words",
+                "[&>td>div]:break-words] [&>td>div]:line-clamp-2 [&>td>div]:text-ellipsis",
               )}
             >
               <td>
-                <div>{problem.name}</div>
+                <div className="text-left">{problem.name}</div>
               </td>
               <td>
-                <div>{problem.language}</div>
+                <div className="text-left">{problem.language}</div>
               </td>
               <td>
                 <div>{problem.best_score}</div>
