980785f9ccc254075163ecfc1f853e00dcf7fc7d
Tuấn Khanh Bùi <54497316+khuongdz@users.noreply.github.com>
2023-07-02 13:05:33 +0700

    reformat home components

---
+++ src/app/(home)/Announcements.tsx
@@ -0,0 +1,12 @@
+export const Announcements = () => {
+  return (
+    <>
+      <div className="w-full">
+        <div className="w-full border-b-2 border-slate-700">
+          <h1 className="m-5 mt-2 block text-3xl">Announcements</h1>
+        </div>
+        <div className="p-2">Welcom to Codefun Debug</div>
+      </div>
+    </>
+  );
+};
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 
 import type { UserRanking } from "@/shared/types";
 
+import { Announcements } from "./Announcements";
+import { Rankings } from "./Rankings";
 export const metadata: Metadata = {
   title: "Home",
 };
@@ -14,49 +16,6 @@ const getTopTenGlobal = async () => {
   }
   const data = (await res.json()) as Array<UserRanking>;
   return data;
-};
-
-const Announcements = () => {
-  return (
-    <>
-      <div className="w-full">
-        <div className="w-full border-b-2 border-slate-700">
-          <h1 className="m-5 mt-2 block text-3xl">Announcements</h1>
-        </div>
-        <div className="p-2">Welcom to Codefun Debug</div>
-      </div>
-    </>
-  );
-};
-
-const Rankings = async ({ data }: { data: Array<UserRanking> }) => {
-  return (
-    <div className="h-auto overflow-hidden rounded-md border-2 border-gray-300">
-      <h2 className="border-b-[1px] bg-gray-300 p-3 text-left text-xl font-semibold text-slate-700">
-        Top users
-      </h2>
-      <table className="w-full border-spacing-y-4 px-2">
-        <thead>
-          <tr className="[&>th]:border-b-2 [&>th]:p-2">
-            <th className="text-right">#</th>
-            <th className="text-center">User</th>
-            <th className="text-right">Score</th>
-          </tr>
-        </thead>
-        <tbody className="divide-y-2 divide-slate-200">
-          {data.map((user) => {
-            return (
-              <tr key={`ranking-user-${user.id}`} className="[&>td]:p-2">
-                <td className="text-right">{user.rank}</td>
-                <td className="text-center">{user.name}</td>
-                <td className="text-right">{Math.round(user.points * 100) / 100}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
-  );
 };
 
 const Page = async () => {
