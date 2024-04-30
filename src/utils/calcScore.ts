1cc05f8b1271d3b200eee99d3b40c4ad383eced1
kbnopro <tuankhanhb00@gmail.com>
2024-04-30 17:29:22 +0700

    simplify score formula

--- src/utils/calcScore.ts
+++ src/utils/calcScore.ts
@@ -1,19 +1,13 @@
 export const calcScore = async ({
-  problemScore,
   submissionScore,
   minDiff,
   submissionDiff,
 }: {
-  problemScore: number;
   submissionScore: number;
   minDiff: number;
   submissionDiff: number;
 }) => {
-  const increasedScore = Math.max(0, submissionScore - problemScore);
-  const scorePercentage = increasedScore / (100 - problemScore);
-
-  const diffPercentage = Math.max(0, 1 - ((submissionDiff - minDiff) * 5) / 100);
-
-  const newScore = scorePercentage * diffPercentage * 100;
-  return Math.round(newScore * 1e5) / 1e5;
+  const reductionPercentage = (Math.max(0, submissionDiff - minDiff) * 5) / 100;
+  const newScore = submissionScore * Math.max(0, 1 - reductionPercentage);
+  return newScore;
 };
