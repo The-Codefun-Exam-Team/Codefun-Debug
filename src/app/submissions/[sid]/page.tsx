0dedfbd6e8acc02c5a397fad95e1c9f083aba3f7
kbnopro <tuankhanhb00@gmail.com>
2024-07-05 17:28:14 +0700

    refactor: rewrite get submission

--- src/app/submissions/[sid]/page.tsx
+++ src/app/submissions/[sid]/page.tsx
@@ -10,7 +10,11 @@ export const metadata: Metadata = {
 
 const Page = async ({ params: { sid } }: { params: { sid: string } }) => {
   const submissionId = parseInt(sid);
-  const submissionData = await getSubmission(submissionId);
+  const submissionQuery = await getSubmission(submissionId);
+  if (!submissionQuery.ok) {
+    throw new Error(submissionQuery.message);
+  }
+  const submissionData = submissionQuery.data;
 
   return (
     <div className="mx-auto flex w-full flex-col items-start gap-6 self-stretch px-3 py-5 md:max-w-7xl md:flex-row md:gap-4 md:px-2 md:py-10 lg:gap-8 lg:px-4">
--- src/features/submissions/queries/getSubmission.ts
+++ src/features/submissions/queries/getSubmission.ts
@@ -1,66 +1,101 @@
 import prisma from "@database/prisma/instance";
-import { unstable_noStore } from "next/cache";
 
 import type { DetailedSubmissionsInfo } from "@/features/submissions";
-import { LANGUAGES_DICT, type UserDisplayInfo } from "@/types";
-import { parseJudge } from "@/utils";
+import {
+  type FunctionReturnType,
+  LANGUAGES_DICT,
+  type UserDisplayInfo,
+} from "@/types";
+import { gravatarFromEmail, handleCatch, parseJudge } from "@/utils";
 
 export const getSubmission = async (
   id: number,
-): Promise<DetailedSubmissionsInfo> => {
-  unstable_noStore();
+): Promise<FunctionReturnType<DetailedSubmissionsInfo>> => {
+  try {
+    const query = await prisma.debugSubmissionQuery.findUniqueOrThrow({
+      where: {
+        id,
+      },
+      select: {
+        id: true,
+        score: true,
+        diff: true,
+        result: true,
+        submitTime: true,
+        runtime: true,
+        source: true,
+        debugProblemCode: true,
+        debugProblemJudge: true,
+        debugProblemLanguage: true,
+        debugProblemSource: true,
+        debugSubmissionJudge: true,
+        username: true,
+        userDisplayName: true,
+        userGroupName: true,
+        userStatus: true,
+        userScore: true,
+        userRatio: true,
+        userRank: true,
+        userEmail: true,
+      },
+    });
 
-  const query = await prisma.debugSubmissionQuery.findUniqueOrThrow({
-    where: {
-      id,
-    },
-    select: {
-      id: true,
-      score: true,
-      diff: true,
-      result: true,
-      submitTime: true,
-      runtime: true,
-      source: true,
-      debugProblemCode: true,
-      debugProblemJudge: true,
-      debugProblemLanguage: true,
-      debugProblemSource: true,
-      debugSubmissionJudge: true,
-      username: true,
-      userDisplayName: true,
-      userGroupName: true,
-      userStatus: true,
-      userScore: true,
-      userRatio: true,
-      userRank: true,
-    },
-  });
-  return {
-    id: query.id,
-    scoreInfo: {
-      score: query.score,
-      diff: query.diff,
-      result: query.result,
-    },
-    submitTime: query.submitTime,
-    runtime: query.runtime,
-    source: query.source,
-    debugProblem: {
-      debugProblemCode: query.debugProblemCode,
-      judge: parseJudge(query.debugProblemJudge),
-      language: LANGUAGES_DICT[query.debugProblemLanguage],
-      source: query.debugProblemSource,
-    },
-    judge: parseJudge(query.debugSubmissionJudge),
-    user: {
-      username: query.username,
-      displayName: query.userDisplayName,
-      groupName: query.userGroupName,
-      status: query.userStatus,
-      score: query.userScore?.toFixed(2) ?? null,
-      ratio: query.userRatio?.toNumber() ?? null,
-      rank: Number(query.userRank),
-    } as UserDisplayInfo,
-  };
+    const user = (): UserDisplayInfo => {
+      if (query.userStatus === "banned") {
+        return {
+          username: query.username,
+          displayName: query.userDisplayName,
+          groupName: query.userGroupName,
+          status: "banned",
+          avatar: gravatarFromEmail(query.userEmail),
+          ratio: null,
+          score: null,
+          rank: null,
+        } satisfies UserDisplayInfo;
+      }
+      if (
+        query.userScore === null ||
+        query.userRatio === null ||
+        query.userRank === null
+      ) {
+        throw new Error("Internal Server Error");
+      }
+      return {
+        username: query.username,
+        displayName: query.userDisplayName,
+        groupName: query.userGroupName,
+        status: "normal",
+        score: query.userScore.toString(),
+        ratio: query.userRatio.toNumber(),
+        rank: Number(query.userRank),
+        avatar: gravatarFromEmail(query.userEmail),
+      } satisfies UserDisplayInfo;
+    };
+
+    const data = {
+      id: query.id,
+      scoreInfo: {
+        score: query.score,
+        diff: query.diff,
+        result: query.result,
+      },
+      submitTime: query.submitTime,
+      runtime: query.runtime,
+      source: query.source,
+      debugProblem: {
+        debugProblemCode: query.debugProblemCode,
+        judge: parseJudge(query.debugProblemJudge),
+        language: LANGUAGES_DICT[query.debugProblemLanguage],
+        source: query.debugProblemSource,
+      },
+      judge: parseJudge(query.debugSubmissionJudge),
+      user: user(),
+    };
+    return {
+      ok: true,
+      data: data,
+    };
+  } catch (e) {
+    return handleCatch(e);
+  }
 };
