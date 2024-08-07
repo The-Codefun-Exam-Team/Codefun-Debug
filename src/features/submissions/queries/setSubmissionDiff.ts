f30de829ab077600f3529a8fb9d9421aacb1677d
Khanh Bùi <54497316+kbnopro@users.noreply.github.com>
2024-08-07 10:45:52 +0700

    fix: debug submission judge can be null (#80)

--- src/features/submissions/queries/setSubmissionDiff.ts
+++ src/features/submissions/queries/setSubmissionDiff.ts
@@ -1,12 +1,9 @@
 import prisma from "@database/prisma/instance";
-import { unstable_noStore } from "next/cache";
 
 export const setSubmissionDiff = async (
   debugSubmissionId: number,
   diff: number,
 ) => {
-  unstable_noStore();
-
   await prisma.debugSubmissions.update({
     where: {
       id: debugSubmissionId,
