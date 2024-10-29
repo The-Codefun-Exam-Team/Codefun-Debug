8fb535336fbe9046e6843e42942f0ad79c3dd23a
kbnopro <tuankhanhb00@gmail.com>
2024-10-29 13:30:00 +0700

    feat: api querying debug submission

---
+++ src/app/api/submission/[id]/route.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+
+import { getSubmission } from "@/features/submissions";
+
+export const GET = async (
+  _req: Request,
+  { params: { id } }: { params: { id: string } },
+) => {
+  try {
+    const idInt = parseInt(id);
+    if (isNaN(idInt)) {
+      return NextResponse.json({ error: "Invalid ID" }, { status: 404 });
+    }
+    const submission = await getSubmission(idInt);
+    if (!submission.ok) {
+      return NextResponse.json(
+        { error: submission.message },
+        { status: submission.status },
+      );
+    }
+    return NextResponse.json(submission.data, { status: 200 });
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 },
+    );
+  }
+};
